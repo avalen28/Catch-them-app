fbb4559dd5acc185c3af201f1890f95f0fa8d8b7
Alberto Valenzuela <avalen28@gmail.com>
2023-08-12 14:20:42 +0200

    Adds api call logic and interfaces

--- src/views/SearchView.tsx
+++ src/views/SearchView.tsx
@@ -1,12 +1,24 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
+import { PokemonInfo } from '../interfaces/pokemon';
 import PokemonCard from '../components/PokemonCard';
 import SearchInput from '../components/SearchInput';
 
 const SearchView = () => {
-    const [pokemon, setPokemon] = useState(null)
+    const [pokemon, setPokemon] = useState({})
 
     const handleSearchPokemon = (searchPokemon: string) => {
-        
+        getPokemonFromApi(searchPokemon)
+
+    }
+    const getPokemonFromApi = async (searchPokemon: string) => {
+        try {
+            const dataFromApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchPokemon}/`);
+            const pokemonFromApi: PokemonInfo = dataFromApi.data;
+            setPokemon(pokemonFromApi)
+        } catch (error) {
+            console.error(error)
+        }
     }
     return (
         <div>
