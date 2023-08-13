00a7c39358c2c1a3b71e6633ed3e40cd34aa0006
Alberto Valenzuela <avalen28@gmail.com>
2023-08-13 19:56:19 +0200

    Adds logic to print stats with first uppercase

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -7,7 +7,7 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemonProp }) => {
     const higestStat: number = pokemonProp?.stats.map(elem => elem.base_stat).sort((a, b) => b - a)[0] || 0;
     const principalType: string = pokemonProp?.types[0].type.name || ""
     const principalTypeColor = pokemonType.find(elem => elem.type === principalType)?.color || ""
-    const secondaryPrincipalTypeColor = pokemonType.find(elem=> elem.type === principalType)?.secondaryColor || ""
+    const secondaryPrincipalTypeColor = pokemonType.find(elem => elem.type === principalType)?.secondaryColor || ""
 
     return (
         <div className='pb-8 w-full'>
@@ -16,10 +16,8 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemonProp }) => {
                     <h2 className='text-5xl text-gray-500'>{pokemonProp.name.toUpperCase()}</h2>
                     <img className="w-96" src={pokemonProp.sprites.other['official-artwork'].front_default} alt="Pokemon default img" />
                 </div>
-
                 <div className='flex justify-between pt-8 w-2/3'>
-                    {/* 
-                    first block */}
+                    {/* first block */}
                     <div className='text-gray-500 w-1/2 flex flex-col gap-4'>
                         <p className='flex w-full justify-between'><span className="w-1/4 text-right">ID</span> <span className='w-2/3 text-left'>{"#" + pokemonProp.id}</span></p>
                         <p className='flex w-full justify-between'><span className="w-1/4 text-right" >Height</span> <span className='w-2/3 text-left'>{pokemonProp.height}</span> </p>
@@ -31,14 +29,11 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemonProp }) => {
                                 <p className={`${secondaryPrincipalTypeColor} px-1 border-1 border-solid border-${secondaryPrincipalTypeColor} rounded-md text-center pl-4 pr-4 pt-1 pb-1 text-slate-400`}>{pokemonProp.abilities[1]?.ability?.name.toUpperCase()}</p>
                             </div>
                         </div>
-
-
-
                         <div className='flex w-full justify-between'>
                             <p className="w-1/4 text-right" >Type</p>
                             <div className='flex w-2/3 gap-2 flex-wrap'>
                                 <div className='flex w-full justify-start gap-5'>
-                                    {pokemonProp.types.map((elem,i) => {
+                                    {pokemonProp.types.map((elem, i) => {
                                         const typeColor = pokemonType.find(type => type.type === elem.type.name)!.color;
                                         const typeIcon = pokemonType.find(type => type.type === elem.type.name)!.img;
                                         return (
@@ -58,16 +53,14 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemonProp }) => {
                         {pokemonProp.stats.map((elem) => {
                             return (
                                 <div className='flex justify-between'>
-                                    <p className='w-1/3 text-left'>{elem.stat.name}</p>
+                                    <p className='w-1/3 text-left'>{elem.stat.name === "hp" ? "HP" : elem.stat.name.charAt(0).toUpperCase() + elem.stat.name.slice(1)}</p>
                                     <div className={`bg-gray-200 w-2/3 border-1 border-solid rounded-md`}>
                                         <div className={`${secondaryPrincipalTypeColor} text-right pr-2 text-slate-400 px-1 border-1 border-solid border-${secondaryPrincipalTypeColor} rounded-md`} style={{ width: `${elem.base_stat / higestStat! * 100}%` }}>{elem.base_stat}</div>
                                     </div>
-
                                 </div>
                             )
                         })}
                     </div>
-
                 </div>
 
             </div>)}
